395b5836c633d7e34cadeda741e2bf2d11ebc2e5
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-01-02 15:53:06 +0000

    needs file param

--- .github/actions/_shared/python/shared/pkg/cli/helpers.py
+++ .github/actions/_shared/python/shared/pkg/cli/helpers.py
@@ -9,4 +9,4 @@ def results(outputs:dict, is_github:bool):
         print(f"{k}={v}")
         if is_github:
             with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-                print(f"{k}={v}", fh)
+                print(f"{k}={v}", file=fh)
