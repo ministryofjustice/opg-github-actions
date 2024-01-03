2ba4c19849933988cc7253fd33ae31d1a340424c
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-01-03 18:24:11 +0000

    remove old python setup

--- .github/actions/_shared/python/shared/pkg/__init__.py
+++
@@ -1,5 +0,0 @@
-from .cli import *
-from .github import *
-from .rand import *
-from .semver import *
-from .tag import *
--- .github/actions/_shared/python/shared/pkg/rand/helpers.py
+++
@@ -1,6 +0,0 @@
-import random
-import string
-
-def rand(n:int):
-    """Generate a random string of n length using lowercase and digits."""
-    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=n))
