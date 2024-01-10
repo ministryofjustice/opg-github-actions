cdbe84eb3fbee52166d5112731621112c6474c79
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-01-10 16:22:52 +0000

    add extra string test and update workflow to run all tests

--- python/tests/common/test_strhelper.py
+++ python/tests/common/test_strhelper.py
@@ -19,3 +19,21 @@ def test_str_to_bool(expected:bool, source:bool|str, default:bool) -> None:
     actual:bool = sth.str_to_bool(source, default)
 
     assert (expected == actual) == True
+
+
+@pytest.mark.parametrize(
+    "test_data,expected",
+    [
+        ("acleanstring", "acleanstring"),
+        ("a string with spaces", "astringwithspaces"),
+        ("other-characters1-*/?-$", "othercharacters1"),
+        ("MixedStr/6/':#", "mixedstr6")
+    ]
+)
+def test_safe(test_data:str, expected:str) -> None:
+    """
+    Test the safe conversion
+    """
+    actual:bool = sth.safe(test_data)
+
+    assert (expected == actual) == True
