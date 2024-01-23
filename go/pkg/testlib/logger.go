6daf2378911e714b62465eac97a934f89f1c097c
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-01-23 09:23:46 +0000

    turn on logging to std out

--- go/pkg/testlib/logger.go
+++ go/pkg/testlib/logger.go
@@ -1,6 +1,7 @@
 package testlib
 
 import (
+	"io"
 	"log/slog"
 	"os"
 )
@@ -9,14 +10,15 @@ var (
 	DefaultOptions = &slog.HandlerOptions{Level: slog.LevelError}
 	InfoOptions    = &slog.HandlerOptions{Level: slog.LevelInfo}
 	VerboseOptions = &slog.HandlerOptions{Level: slog.LevelDebug}
-	//destination    = io.Discard
-	destination = os.Stdout
+	ignore         = io.Discard
+	stdout         = os.Stdout
 )
 
 func Testlogger(opts *slog.HandlerOptions) {
+	var handler *slog.TextHandler
 	if opts == nil {
 		opts = DefaultOptions
 	}
-	handler := slog.NewTextHandler(destination, opts)
+	handler = slog.NewTextHandler(stdout, opts)
 	slog.SetDefault(slog.New(handler))
 }
