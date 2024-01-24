2a9cd9356eb67fd49c74ab9425b6186b557ef581
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-01-24 14:51:00 +0000

    have to split the process

---
+++ .github/actions/release-download.sh
@@ -0,0 +1,115 @@
+#!/usr/bin/env bash
+
+
+actionRepo="${GH_ACTION_REPOSITORY}"
+actionRef="${GH_ACTION_REF}"
+basePath="${GH_WORKSPACE}/../opg-gha"
+localBuildPath="${GH_WORKSPACE}/../opg-gha-build"
+artifactPath="${basePath}/releases"
+tarball="release.tar.gz"
+os=$(uname | tr '[:upper:]' '[:lower:]')
+arch=$(uname -m)
+hostBuild="${os}_${arch}"
+ok=">ok<"
+
+####
+# These vars will be passed along to determine next action
+RELEASE=""
+SELF_BUILD=""
+####
+
+# Look for the reference in the existing releases
+# - if we find that, we'll use that directly
+echo -n "Trying direct release download using [${actionRef}] [${actionRepo}]..."
+releases=$(gh release list --exclude-drafts=false --exclude-pre-releases=false -R "${actionRepo}")
+listed=$( echo "${releases}" | grep "^${actionRef}" && echo "${ok}")
+
+set -e
+set -o pipefail
+
+# If ref is in the release list, then we can download
+# and then move the artifact
+if [ "${listed}" == "${ok}" ]; then
+    echo " ✅"
+    cd ${basePath}
+    mkdir -p ${artifactPath}
+    echo -e "Downloading existing release [${actionRef}]..."
+    # download the release tar ball
+    gh release download "${actionRef}" -R "${actionRepo}" --clobber
+    # move them
+    echo -e "Moving tarball to [${artifactPath}]"
+    mv *.tar.gz ${artifactPath}
+    # expand the tarball
+    cd ${artifactPath}
+    echo -e "Expanding tarball [${artifactPath}/${tarball}]"
+    tar -xzvf ${tarball}
+    # look for this arch
+    echo -n "Looking for binary for this runner [${artifactPath}/${hostBuild}]..."
+    if [ -x "${hostBuild}" ]; then
+        echo " ✅"
+        RELEASE="${artifactPath}/${$hostBuild}"
+        echo -e "Set release: [${RELEASE}]"
+    else
+        echo " ❌"
+        echo -e "Failed to find binary for this runner, will trigger the self build..."
+        listed=""
+    fi
+fi
+# If we failed to download the artifact using the action_ref directly
+# then its likely someone has used a prerelease or a git hash ref 
+# to pass to us 
+# In that case, we need to download the repo to a new location so
+# that it can be built
+if [ "${listed}" != "${ok}" ]; then
+    echo " ❌"
+    # build from local 
+    echo -e "Cloning action repostitory [${actionRepo}] to [${localBuildPath}] ..."
+    gh repo clone ${actionRepo} ${localBuildPath} 
+    
+    cd ${localBuildPath}
+    pwd
+    ls -la
+    # checkout to the git ref
+    checkout=$(git checkout -q -f ${actionRef} -- 2> /dev/null && echo "${ok}")
+
+    if [ "${checkout}" == "${ok}" ]; then
+        echo -e "Checked out local repo to [${actionRef}] [${localBuildPath}]"
+        SELF_BUILD="${localBuildPath}"
+    else
+        err="ERROR: failed to checkout [${actionRef}]"
+        echo -e "${err}"
+        echo -e "${err}" >&2
+        exit 1
+    fi
+
+  
+fi
+
+export RELEASE=${RELEASE}
+export SELF_BUILD=${SELF_BUILD}
+
+echo "SELF_BUILD=${SELF_BUILD}" >> $GITHUB_OUTPUT
+echo "RELEASE=${RELEASE}" >> $GITHUB_OUTPUT
+
+# make sure we have an artifact that matches this runner
+
+# cd ${artifactPath}
+# ls -la
+# pwd
+# if [ ! -r "${tarball}" ]; then
+#     err="ERROR: could not find a readable tar ball at [${artifactPath}/${tarball}]"
+#     echo -e "${err}" >&2
+#     exit 1
+# fi
+
+# echo -e "Expanding tar ball [${tarball}]"
+# tar -xzvf ${tarball}
+
+# if [ ! -x "${hostBuild}" ]; then
+#     err="ERROR: unable to find executable at [${artifactPath}/${hostBuild}]"
+#     echo -e "${err}" >&2
+#     exit 1
+# fi
+
+# echo -e "release=${artifactPath}/${hostBuild}"
+# echo "release=${artifactPath}/${hostBuild}" >> $GITHUB_OUTPUT
