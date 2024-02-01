633428df47a69b0c495f27afd49030a7aff723ad
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-02-01 10:01:26 +0000

    add latest tag as well so it generates comparison link

--- .github/actions/release-notes.sh
+++ .github/actions/release-notes.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 target="${GH_REPO}"
 commit="${GH_COMMIT}"
+previous="${LAST_TAG}"
 tag="${TAGNAME}"
+debug="${DEBUG}"
 maxLength=124000
 
 accept="Accept: application/vnd.github+json"
 ver="X-GitHub-Api-Version: 2022-11-28"
 endpoint="/repos/${target}/releases/generate-notes"
-tagParam="-f tag_name='${targetTag}' "
-commitParam="-f target_commitish=${commit}"
+tagParam="-f tag_name=${tag} -f previous_tag_name=${previous} "
+commitParam="-f target_commitish=${commit} "
 
 echo -n "Generating release notes..."
 generatedNotes=$(gh api --method POST -H "${accept}" -H "${ver}" ${endpoint} ${tagParam} ${commitParam} 2>/dev/null )
@@ -35,6 +37,11 @@ else
     echo " ✅"
 fi
 
+if [ "${debug}" == "1" ]; then
+    echo -e "==="
+    echo -e "${body}"
+    echo -e "==="
+fi
 # export variables back for use in workflow
 export RELEASE_BODY=${body}
 echo "RELEASE_BODY=${RELEASE_NOTE_BODY}" >> $GITHUB_OUTPUT
