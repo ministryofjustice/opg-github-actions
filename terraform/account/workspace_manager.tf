68cc0270d048186c792711b33f901ec5bd88a103
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-03-06 17:44:03 +0000

    Use the workspace manager forked into the moj org

--- terraform/account/workspace_manager.tf
+++ terraform/account/workspace_manager.tf
@@ -1,4 +1,5 @@
 module "workspace_cleanup" {
-  source  = "github.com/TomTucka/terraform-workspace-manager/terraform/workspace_cleanup"
+  # previous: github.com/TomTucka/terraform-workspace-manager/terraform/workspace_cleanup
+  source  = "github.com/ministryofjustice/opg-terraform-workspace-manager/terraform/workspace_cleanup"
   enabled = true
 }
