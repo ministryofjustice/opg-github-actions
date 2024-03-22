f4f864efb9a3afab9edc127d4c30f037735f3646
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-03-22 14:02:07 +0000

    format of source

--- terraform/account/workspace_manager.tf
+++ terraform/account/workspace_manager.tf
@@ -1,5 +1,5 @@
 module "workspace_cleanup" {
   # previous: github.com/TomTucka/terraform-workspace-manager/terraform/workspace_cleanup
-  source  = "git@github.com:ministryofjustice/opg-terraform-workspace-manager//terraform/workspace_cleanup?ref=v0.3.2"
+  source  = "github.com/ministryofjustice/opg-terraform-workspace-manager//terraform/workspace_cleanup?ref=v0.3.2"
   enabled = true
 }
