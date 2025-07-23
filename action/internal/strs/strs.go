5d90e050e42eba526ce411a1d0842b8bf0ac58fc
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2025-07-23 09:58:58 +0100

    some go and make file

---
+++ action/internal/strs/strs.go
@@ -0,0 +1,41 @@
+package strs
+
+import (
+	"regexp"
+	"strings"
+)
+
+const allowCharacters string = "[^a-zA-Z0-9]+"
+
+// Clean converts a string to only include alpha numerics characters, so removes
+// forward slashes and so on
+func Clean(s string) (safe string) {
+	var (
+		err   error
+		exp   *regexp.Regexp
+		lower string = strings.ToLower(s)
+	)
+	if exp, err = regexp.Compile(allowCharacters); err != nil {
+		return
+	}
+	safe = exp.ReplaceAllString(lower, "")
+	return
+}
+
+// Truncate the string to at most maxlength characters
+func Truncate(s string, maxLength int) (short string) {
+	if len(s) > maxLength {
+		s = s[0:maxLength]
+	}
+	short = s
+	return
+}
+
+// Safe cleans the string to only allowed characters and then truncates
+// that result to desired length - returning both values
+func Safe(s string, maxLength int) (safeAndShort string, safe string) {
+	safe = Clean(s)
+	safeAndShort = Truncate(safe, maxLength)
+	return
+
+}
